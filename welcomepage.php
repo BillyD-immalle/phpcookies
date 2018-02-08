7603e1ff36d1559ede5dd2373204885d5ac7bae1
BillyD-immalle <billy.dhoore@immalle.be>
2018-02-08 21:52:58 +0100

    fix typos

--- welcomepage.php
+++ welcomepage.php
@@ -1,5 +1,5 @@
 <h3>What is your name?</h3>
-<form action="name.php" method="POST">
+<form action="username.php" method="POST">
 	<input type="text" name="username"/>
 	<input type="submit" value="ok"/>
 </form>
